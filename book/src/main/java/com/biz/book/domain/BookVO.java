301747a35385e54bbff505bb597d4d2b1d139d87
smskit726 <smskit726@gmail.com>
2020-11-20 12:36:50 +0900

    Boot-Book [IntelliJ]

---
+++ book/src/main/java/com/biz/book/domain/BookVO.java
@@ -0,0 +1,36 @@
+package com.biz.book.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tbl_book")
+public class BookVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(name = "str_name")
+    private String strName;
+
+    @Column(columnDefinition = "VARCHAR(125)")
+    private String title;
+
+    @Column(length = 30)
+    private String author;
+
+    @Column(length = 125)
+    private String comp;
+
+    @Column(columnDefinition = "INT DEFAULT 0")
+    private int price;
+}
